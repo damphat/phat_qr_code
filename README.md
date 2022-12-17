4e918ab5046822ba6d534b60d8b3862699c9ba2e
Phat <damphat@gmail.com>
2022-12-17 16:30:11 +0700

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# phat_qr_code
+
+- [x] generate javascript/HTML app
+- [x] launcher icons
+- [x] launch the extension 
+- [x] add qr_code library
+- [ ] add UI
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String data = "phat";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        QrImage(
+          data: data,
+        ),
+      ]),
+    );
+  }
+}
